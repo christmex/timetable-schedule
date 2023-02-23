6f05b34f7a47736c6813dd2a8327d9e11e0fc8ed
christmex <christmex@yahoo.com>
2023-02-23 13:03:38 +0700

    Add featureBulkChangeLesson

---
+++ app/Http/Livewire/Widgets/BulkChangeLesson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Widgets;
+
+use Livewire\Component;
+
+class BulkChangeLesson extends Component
+{
+    public function render()
+    {
+        return view('livewire.widgets.bulk-change-lesson');
+    }
+}
